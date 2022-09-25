44a9dfb86ed137b01eabd20dd7189756606e1f96
Xecozz <94707887+Xecozz@users.noreply.github.com>
2022-09-25 19:19:42 +0200

    v1

---
+++ js/screens.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.btn-navigation').click(function(){
+      $('.mygallery').toggleClass('isOpen');
+    });
+  });
+
+  $(document).ready(function(){
+    $('.btn-navigation').click(function(){
+      $('.navigation').toggleClass('isOpen');
+    });
+  });
---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.btn-navigation').click(function(){
+      $('.navigation').toggleClass('isOpen');
+    });
+  });
+
+  $(document).ready(function(){
+    $('.btn-navigation').click(function(){
+      $('.header').toggleClass('isOpen');
+    });
+  });
+
